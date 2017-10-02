2378aebc82243dbc1d938aee8f18a1047460eab0
Gaydukov Dmitriy <gaydukov89@gmail.com>
2017-10-02 20:37:51 +0300

    add event memoization

--- src/modules/main/main.js
+++ src/modules/main/main.js
@@ -2,9 +2,12 @@
 
 /**
  * Main page component
+ * Example how to use WeakMap & function memoizaition
+ * If we pass onClick={this.onClick.bind(this,i)}, then on every state change our Button component will re-render
+ * In order to avoid this, we pass preBound function to this
  */
 
-import React from 'react';
+import React, {PureComponent} from 'react';
 
 export default class Main extends React.Component{
     constructor(props){
@@ -12,10 +15,30 @@ export default class Main extends React.Component{
         this.state = {
             counter: 1,
         }
+        this.cache = new WeakMap();
+        this.onClick = this.onClick.bind(this);
     }
 
-    click(){
-        this.setState({counter: this.state.counter+1});
+    renderButton(i){
+        const item = {id: i};
+        if(this.cache.has(item)){
+            return this.cache.get(item)
+        }
+        const component = (
+            <Button
+                key={item.id}
+                i={item.id}
+                title={`click ${i}`}
+                onClick={this.onClick}
+            />
+        )
+        this.cache.set(item, component)
+        return component
+    }
+
+
+    onClick(i){
+        this.setState({counter: i});
     }
 
     render(){
@@ -23,8 +46,22 @@ export default class Main extends React.Component{
             <div>
                 <h1>Main Page</h1>
                 <div>{this.state.counter}</div>
-                <button onClick={::this.click}>add</button>
+                {[...new Array(10)].map((item,i)=>this.renderButton(i))}
             </div>
+        )
+    }
+}
+
+class Button extends PureComponent{
+    render(){
+        const {
+            title,
+            onClick,
+            i
+        } = this.props;
+        console.log("render Button")
+        return(
+            <button onClick={()=>{onClick(i)}}>{title}</button>
         )
     }
 }
