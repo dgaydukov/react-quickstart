3991da7d0b84add442ddef33cf57b6dd1b50c56d
Dmitiry <gaydukov89@gmail.com>
2018-08-24 14:01:54 +0300

    clean npm folder

--- npm/build/build.prod.js
+++ npm/build/build.prod.js
@@ -9,7 +9,7 @@ const path = require('path');
 const uglifycss = require('uglifycss');
 
 const helper = {
-    guid: ()->{
+    guid: ()=>{
         function s4() {
             return Math.floor((1 + Math.random()) * 0x10000)
                 .toString(16)
